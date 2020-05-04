5240495aec403565e7f829b430e42b0bac75e5a8
spaniolhoratiu <45192354+spaniolhoratiu@users.noreply.github.com>
2020-05-04 20:36:55 +0300

    added GUI

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import test_accuracy_cnn_model
+import pytorch_training_cnn_model
+import image_prediction_using_model
+from tkinter import filedialog
+from tkinter import *
 
 
 def main():
     print("Training model...")
-    # train_and_build(200)
-    while True:
-        inputString = input('Input: ')
-        if inputString == "predict image":
-            print('prediction!')  # to be replaced with opening image, call prediction function, convert from int to
-            # appropriate name
-        else:
-            if inputString == 'test accuracy':
-                test_accuracy_cnn_model.test_accuracy()
-            else:
-                if inputString == 'exit':
-                    inputString = input('Are you sure? [y/n]')
-                    if inputString == 'y':
-                        break
-                else:
-                    print(inputString)
+    # pytorch_training_cnn_model.train_and_build(200)
+    print("Model finished training.")
+    window = Tk()
+    window.resizable(False, False)
+    window.geometry('350x125')
+    window.title("GLaDOS")
+    title_label = Label(window, text="Flower Prediction", font=("Arial Bold", 30))
+    title_label.grid(column=0, row=0)
+    prediction_label = Label(window, text="Prediction", font=("Arial Bold", 15))
+    prediction_label.grid(column=0, row=3)
+
+    def predict_button_clicked():
+        prediction_label.configure(text="Processing...")
+        window.filename = filedialog.askopenfilename(
+            initialdir="D:/Facultate/AN III/Semester 2/Intelligent Systems/venv/flowers", title="Select file",
+            filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+        if not window.filename:
+            prediction_label.configure(text="No file selected. :(")
+            return
+
+        path_to_file = window.filename[56:]
+        flower_name = image_prediction_using_model.predict(path_to_file)
+        prediction_label.configure(text="My prediction is: " + flower_name)
+
+    predict_button = Button(window, text="Predict", command=predict_button_clicked)
+    predict_button.grid(column=0, row=1)
+
+    window.mainloop()
 
 
 if __name__ == "__main__":
